eb397f073497532f975cbd6cd15cc1b067f66a70
Minh_Dung <nguyenleminhdung2912@gmail.com>
2024-02-21 10:16:22 +0700

    Done Entity Done Repo Done Service Done DBUtil Done BE Configuration 1/5 of the controllers

--- src/main/java/com/swp391/webapp/Entity/Service_Detail_DTO.java
+++ src/main/java/com/swp391/webapp/Entity/Service_Detail_DTO.java
@@ -12,4 +12,20 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Service_Detail_DTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Service_Detail_ID")
+    private Long serviceDetailId;
+
+    @ManyToOne
+    @JoinColumn(name = "Cart_ID", nullable = false)
+    private CartDTO cart;
+
+    @ManyToOne
+    @JoinColumn(name = "service_ID", nullable = false)
+    private ServiceDTO service;
+
+    // Constructors, getters, and setters
+
+    // Omitted for brevity
 }
